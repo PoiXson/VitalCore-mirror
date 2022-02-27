da56b8449c2ed2631a53eb728801cc3de90c181c
lorenzop <lorenzo@poixson.com>
2022-02-27 02:07:40 -0500

    added chunk classes

---
+++ src/com/poixson/commonbukkit/tools/chunks/ChunkDAO.java
@@ -0,0 +1,88 @@
+package com.poixson.commonbukkit.tools.chunks;
+
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.World;
+import org.bukkit.block.data.BlockData;
+
+import com.poixson.commonbukkit.utils.BukkitUtils;
+import com.poixson.utils.StringUtils;
+
+
+public class ChunkDAO {
+
+	public final World world;
+	public final int chunkX, chunkZ;
+	public final int absX, absZ;
+
+
+
+	public ChunkDAO(final World world, final int chunkX, final int chunkZ) {
+		this.world  = world;
+		this.chunkX = chunkX;
+		this.chunkZ = chunkZ;
+		this.absX   = chunkX * 16;
+		this.absZ   = chunkZ * 16;
+	}
+
+
+
+	public void setBlockJS(final Object material, final Object x, final Object y, final Object z) {
+		if (material == null)
+			return;
+		BlockData mat = null;
+		if (material instanceof String) {
+			final String matStr = StringUtils.ToString(material);
+			if (matStr.length() == 0)
+				return;
+			mat = BukkitUtils.ParseBlockType(matStr);
+			if (mat == null) {
+				System.out.println("Unknown block/material type: " + matStr);
+				return;
+			}
+		} else
+		if (material instanceof BlockData) {
+			mat = (BlockData) material;
+		}
+		if (mat == null) {
+			System.out.println("Unknown block/material type: " +
+					StringUtils.ToString(material) + "  " + material.getClass().getName());
+			return;
+		}
+		final Double xx = (Double) x;
+		final Double yy = (Double) y;
+		final Double zz = (Double) z;
+		this.setBlock(mat, xx.intValue(), yy.intValue(), zz.intValue());
+	}
+	public void setBlocksJS(final Object blocks) {
+		final List<?> list = (List<?>) blocks;
+		for (final Object entry : list) {
+			if (entry == null) continue;
+			@SuppressWarnings("unchecked")
+			final Map<String, Object> map = (Map<String, Object>) entry;
+			final Object type = map.get("type");
+			final Object x    = map.get("x");
+			final Object y    = map.get("y");
+			final Object z    = map.get("z");
+			this.setBlockJS(type, x, y, z);
+		}
+	}
+
+
+
+	// override in gen/pop child classes
+	public void setBlock(final BlockData material, final int x, final int y, final int z) {
+		if (this.world == null) throw new NullPointerException("world");
+		this.world.setBlockData(this.absX+x, y, this.absZ+z, material);
+	}
+
+
+
+	public int getSeed() {
+		return (int) (this.world.getSeed() % 2147483647L);
+	}
+
+
+
+}
