e3ae51116d8e16e2f5d0d7fc5273090e82e0c1bb
lorenzop <lorenzo@poixson.com>
2022-12-14 07:42:56 -0500

    added place() functions to BlockPlotter class and other improvements

--- src/com/poixson/commonbukkit/tools/BlockPlotter.java
+++ src/com/poixson/commonbukkit/tools/BlockPlotter.java
@@ -1,15 +1,21 @@
 package com.poixson.commonbukkit.tools;
 
-import static com.poixson.commonbukkit.utils.LocationUtils.RotateXZ;
+import static com.poixson.commonbukkit.utils.LocationUtils.AxisToValue;
+
+import java.util.HashMap;
 
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Lightable;
+import org.bukkit.block.data.type.Slab;
 import org.bukkit.generator.ChunkGenerator.ChunkData;
 import org.bukkit.generator.LimitedRegion;
 
-import com.poixson.tools.dao.Ixywd;
+import com.poixson.exceptions.RequiredArgumentException;
+import com.poixson.tools.dao.Ixyz;
+import com.poixson.utils.StringUtils;
+import com.poixson.utils.Utils;
 
 
 public class BlockPlotter {
@@ -18,88 +24,270 @@ public class BlockPlotter {
 	public final ChunkData chunk;
 	public final LimitedRegion region;
 
-	public final BlockFace direction;
-	public final int absX, absY, absZ;
-	public final int w, d;
+	public int absX, absY, absZ;
+
+	public HashMap<Character, Material> types = new HashMap<Character, Material>();
+	public HashMap<Character, String> special = new HashMap<Character, String>();
 
 
 
-	public BlockPlotter(final World world, final BlockFace direction,
-			final int absX, final int absY, final int absZ, final int w, final int d) {
-		this(world, null, null, direction, absX, absY, absZ, w, d);
+	public BlockPlotter(final World world) {
+		this(world, 0, 0, 0);
 	}
-	public BlockPlotter(final ChunkData chunk, final BlockFace direction,
-			final int absX, final int absY, final int absZ, final int w, final int d) {
-		this(null, chunk, null, direction, absX, absY, absZ, w, d);
+	public BlockPlotter(final ChunkData chunk) {
+		this(chunk, 0, 0, 0);
 	}
-	public BlockPlotter(final LimitedRegion region, final BlockFace direction,
-			final int absX, final int absY, final int absZ, final int w, final int d) {
-		this(null, null, region, direction, absX, absY, absZ, w, d);
+	public BlockPlotter(final LimitedRegion region) {
+		this(region, 0, 0, 0);
 	}
-	protected BlockPlotter(final World world, final ChunkData chunk,
-			final LimitedRegion region, final BlockFace direction,
-			final int absX, final int absY, final int absZ, final int w, final int d) {
-		if (world == null && chunk == null && region == null)
-			throw new NullPointerException();
+
+	public BlockPlotter(final World world, final int x, final int y, final int z) {
+		if (world == null) throw new NullPointerException();
 		this.world  = world;
+		this.chunk  = null;
+		this.region = null;
+		this.absX = x;
+		this.absY = y;
+		this.absZ = z;
+	}
+	public BlockPlotter(final ChunkData chunk, final int x, final int y, final int z) {
+		if (chunk == null) throw new NullPointerException();
+		this.world  = null;
 		this.chunk  = chunk;
+		this.region = null;
+		this.absX = x;
+		this.absY = y;
+		this.absZ = z;
+	}
+	public BlockPlotter(final LimitedRegion region, final int x, final int y, final int z) {
+		if (region == null) throw new NullPointerException();
+		this.world  = null;
+		this.chunk  = null;
 		this.region = region;
-		this.direction = direction;
-		this.absX = absX;
-		this.absY = absY;
-		this.absZ = absZ;
-		this.w = w;
-		this.d = d;
+		this.absX = x;
+		this.absY = y;
+		this.absZ = z;
 	}
 
 
 
-	public void setBlock(final int x, final int y, final int z, final Material type) {
-		final Ixywd rel = RotateXZ(new Ixywd(x, z, this.w, this.d), this.direction);
-		this.setAbsBlock(rel.x+this.absX, y+this.absY, rel.y+this.absZ, type);
+	public StringBuilder[][] getEmptyMatrix3D(final int size1, final int size2) {
+		final StringBuilder[][] matrix = new StringBuilder[size1][size2];
+		for (int i=0; i<size1; i++) {
+			for (int ii=0; ii<size2; ii++) {
+				matrix[i][ii] = new StringBuilder();
+			}
+		}
+		return matrix;
+	}
+	public StringBuilder[] getEmptyMatrix2D(final int size1) {
+		final StringBuilder[] matrix = new StringBuilder[size1];
+		for (int i=0; i<size1; i++) {
+			matrix[i] = new StringBuilder();
+		}
+		return matrix;
+	}
+	public StringBuilder getEmptyMatrix1D() {
+		return new StringBuilder();
 	}
 
-	public BlockData getBlockData(final int x, final int y, final int z) {
-		final Ixywd rel = RotateXZ(new Ixywd(x, z, this.w, this.d), this.direction);
-		return this.getAbsBlockData(rel.x+this.absX, y+this.absY, rel.y+this.absZ);
+	public StringBuilder[][] getNewMatrix3D(final int size1, final int size2, final int size3) {
+		final StringBuilder[][] matrix = new StringBuilder[size1][size2];
+		for (int i=0; i<size1; i++) {
+			for (int ii=0; ii<size2; ii++) {
+				matrix[i][ii] = new StringBuilder();
+				matrix[i][ii].append(StringUtils.Repeat(size3, ' '));
+			}
+		}
+		return matrix;
 	}
-	public void setBlockData(final int x, final int y, final int z, final BlockData block) {
-		final Ixywd rel = RotateXZ(new Ixywd(x, z, this.w, this.d), this.direction);
-		this.setAbsBlockData(rel.x+this.absX, y+this.absY, rel.y+this.absZ, block);
+	public StringBuilder[] getNewMatrix2D(final int size1, final int size2) {
+		final StringBuilder[] matrix = new StringBuilder[size1];
+		for (int i=0; i<size1; i++) {
+			matrix[i] = new StringBuilder();
+			matrix[i].append(StringUtils.Repeat(size2, ' '));
+		}
+		return matrix;
+	}
+	public StringBuilder getNewMatrix1D(final int size) {
+		final StringBuilder matrix = new StringBuilder();
+		matrix.append(StringUtils.Repeat(size, ' '));
+		return matrix;
 	}
 
 
 
-	protected void setAbsBlock(final int x, final int y, final int z, final Material type) {
+	public void place3D(final String axis, final StringBuilder[][] matrix) {
+		if (Utils.isEmpty(axis)) throw new RequiredArgumentException("axis");
+		if (axis.length() != 3) throw new RuntimeException("Invalid axis length: "+axis);
+		final Ixyz add1 = AxisToValue(axis.charAt(0));
+		final Ixyz add2 = AxisToValue(axis.charAt(1));
+		final Ixyz add3 = AxisToValue(axis.charAt(2));
+		final int size1 = matrix.length;
+		int x = this.absX;
+		int y = this.absY;
+		int z = this.absZ;
+		int xx, yy, zz;
+		int xxx, yyy, zzz;
+		int size2, size3;
+		char c;
+		for (int i=0; i<size1; i++) {
+			xxx = x; yyy = y; zzz = z;
+			size2 = matrix[i].length;
+			for (int ii=0; ii<size2; ii++) {
+				xx = x; yy = y; zz = z;
+				size3 = matrix[i][ii].length();
+				for (int iii=0; iii<size3; iii++) {
+					c = matrix[i][ii].charAt(iii);
+					if (c != 0 && c != ' ') {
+						final Material m = this.types.get(Character.valueOf(c));
+						if (m == null) throw new RuntimeException(String.format("Warning, unknown material: %c", c));
+						final String s = this.special.get(Character.valueOf(c));
+						this.setAbsBlock(x, y, z, m, s);
+					}
+					x += add3.x; y += add3.y; z += add3.z;
+				}
+				x = xx + add2.x; y = yy + add2.y; z = zz + add2.z;
+			}
+			x = xxx + add1.x; y = yyy + add1.y; z = zzz + add1.z;
+		}
+	}
+	public void place2D(final String axis, final StringBuilder[] matrix) {
+		if (Utils.isEmpty(axis)) throw new RequiredArgumentException("axis");
+		if (axis.length() != 2) throw new RuntimeException("Invalid axis length: "+axis);
+		final Ixyz add1 = AxisToValue(axis.charAt(0));
+		final Ixyz add2 = AxisToValue(axis.charAt(1));
+		final int size1 = matrix.length;
+		int x = this.absX;
+		int y = this.absY;
+		int z = this.absZ;
+		int xx, yy, zz;
+		int size2;
+		char c;
+		for (int i=0; i<size1; i++) {
+			xx = x; yy = y; zz = z;
+			size2 = matrix[i].length();
+			for (int ii=0; ii<size2; ii++) {
+				c = matrix[i].charAt(ii);
+				if (c != 0 && c != ' ') {
+					final Material m = this.types.get(Character.valueOf(c));
+					if (m == null) throw new RuntimeException(String.format("Warning, unknown material: %c", c));
+					final String s = this.special.get(Character.valueOf(c));
+					this.setAbsBlock(x, y, z, m, s);
+				}
+				x += add2.x; y += add2.y; z += add2.z;
+			}
+			x = xx + add1.x; y = yy + add1.y; z = zz + add1.z;
+		}
+	}
+	public void place1D(final char axis, final StringBuilder matrix) {
+		final Ixyz add = AxisToValue(axis);
+		final int size = matrix.length();
+		int x = this.absX;
+		int y = this.absY;
+		int z = this.absZ;
+		char c;
+		for (int i=0; i<size; i++) {
+			c = matrix.charAt(i);
+			if (c != 0 && c != ' ') {
+				final Material m = this.types.get(Character.valueOf(c));
+				if (m == null) throw new RuntimeException(String.format("Warning, unknown material: %c", c));
+				final String s = this.special.get(Character.valueOf(c));
+				this.setAbsBlock(x, y, z, m, s);
+			}
+			x += add.x; y += add.y; z += add.z;
+		}
+	}
+
+
+
+	public void type(final char c, final Material m) {
+		this.types.put(Character.valueOf(c), m);
+	}
+	public void type(final char c, final Material m, final String special) {
+		this.type(c, m);
+		this.special.put(Character.valueOf(c), special);
+	}
+
+
+
+	public void setRelBlock(final int x, final int y, final int z, final Material type, final String special) {
+		this.setAbsBlock(x+this.absX, y+this.absY, z+this.absZ, type, special);
+	}
+	public void setRelBlock(final int x, final int y, final int z, final Material type) {
+		this.setAbsBlock(x+this.absX, y+this.absY, z+this.absZ, type);
+	}
+
+	public BlockData getRelBlockData(final int x, final int y, final int z) {
+		return this.getAbsBlockData(x+this.absX, y+this.absY, z+this.absZ);
+	}
+	public void setRelBlockData(final int x, final int y, final int z, final BlockData block) {
+		this.setAbsBlockData(x+this.absX, y+this.absY, z+this.absZ, block);
+	}
+
+
+
+	public void setAbsBlock(final int x, final int y, final int z, final Material type, final String special) {
+		this.setAbsBlock(x, y, z, type);
+		if (Utils.notEmpty(special)) {
+			boolean changed = false;
+			final String sp = (new StringBuilder())
+					.append(',').append(special).append(',')
+					.toString();
+			final BlockData data = this.getAbsBlockData(x, y, z);
+			// slab
+			if (data instanceof Slab) {
+				if (sp.contains(",top,")) {
+					changed = true; ((Slab)data).setType(Slab.Type.TOP);
+				} else
+				if (sp.contains(",bottom,")) {
+					changed = true; ((Slab)data).setType(Slab.Type.BOTTOM);
+				}
+			}
+			// lamp
+			if (data instanceof Lightable) {
+				if (sp.contains(",on,")) {
+					changed = true; ((Lightable)data).setLit(true);
+				} else
+				if (sp.contains(",off,")) {
+					changed = true; ((Lightable)data).setLit(false);
+				}
+			}
+			if (changed) {
+				this.setAbsBlockData(x, y, z, data);
+			}
+		}
+	}
+	public void setAbsBlock(final int x, final int y, final int z, final Material type) {
 		if (this.world != null) {
 			this.world.setType(x, y, z, type);
 		} else
 		if (this.chunk != null) {
-			this.chunk.setBlock(x, y, z, type);
+			this.chunk.setBlock(Math.abs(x%16), y, Math.abs(z%16), type);
 		} else
 		if (this.region != null) {
 			this.region.setType(x, y, z, type);
 		}
 	}
 
-	protected BlockData getAbsBlockData(final int x, final int y, final int z) {
+	public BlockData getAbsBlockData(final int x, final int y, final int z) {
 		if (this.world != null) {
 			return this.world.getBlockData(x, y, z);
 		} else
 		if (this.chunk != null) {
-			return this.chunk.getBlockData(x, y, z);
+			return this.chunk.getBlockData(Math.abs(x%16), y, Math.abs(z%16));
 		} else
 		if (this.region != null) {
 			return this.region.getBlockData(x, y, z);
 		}
 		return null;
 	}
-	protected void setAbsBlockData(final int x, final int y, final int z, final BlockData block) {
+	public void setAbsBlockData(final int x, final int y, final int z, final BlockData block) {
 		if (this.world != null) {
 			this.world.setBlockData(x, y, z, block);
 		} else
 		if (this.chunk != null) {
-			this.chunk.setBlock(x, y, z, block);
+			this.chunk.setBlock(Math.abs(x%16), y, Math.abs(z%16), block);
 		} else
 		if (this.region != null) {
 			this.region.setBlockData(x, y, z, block);
