ccf3e1e7ca06c3ac3a5e79588c8ad4075dc24c2c
lorenzop <lorenzo@poixson.com>
2023-01-13 07:27:18 -0500

    more cleanup and fixes

--- src/com/poixson/commonmc/pxnCommonPlugin.java
+++ src/com/poixson/commonmc/pxnCommonPlugin.java
@@ -2,9 +2,6 @@ package com.poixson.commonmc;
 
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.logging.Logger;
-
-import org.bukkit.ChatColor;
 
 import com.poixson.commonmc.tools.plugin.xJavaPlugin;
 import com.poixson.commonmc.tools.updatechecker.UpdateCheckManager;
@@ -13,10 +10,8 @@ import com.poixson.tools.Keeper;
 
 
 public class pxnCommonPlugin extends xJavaPlugin {
-	public static final String LOG_PREFIX  = "[pxnCommon] ";
-	public static final String CHAT_PREFIX = ChatColor.AQUA + LOG_PREFIX + ChatColor.WHITE;
-	public static final Logger log = Logger.getLogger("Minecraft");
-	public static final int SPIGOT_PLUGIN_ID = 107049;
+	protected static final String LOG_PREFIX  = "[pxnCommon] ";
+	protected static final int SPIGOT_PLUGIN_ID = 107049;
 
 	protected static final AtomicReference<pxnCommonPlugin> instance = new AtomicReference<pxnCommonPlugin>(null);
 	protected final Keeper keeper;
@@ -70,7 +65,7 @@ public class pxnCommonPlugin extends xJavaPlugin {
 			}
 		}
 		if (!instance.compareAndSet(this, null))
-			throw new RuntimeException("Disable wrong instance of plugin?");
+			(new RuntimeException("Disable wrong instance of plugin?")).printStackTrace();
 	}
 
 
