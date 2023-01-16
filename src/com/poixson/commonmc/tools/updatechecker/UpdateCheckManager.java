4dbd783b24dec0c4b5503350f5ba13741c63dc3e
lorenzop <lorenzo@poixson.com>
2023-01-16 16:02:22 -0500

    fixed update checker period - ms not ticks

--- src/com/poixson/commonmc/tools/updatechecker/UpdateCheckManager.java
+++ src/com/poixson/commonmc/tools/updatechecker/UpdateCheckManager.java
@@ -24,7 +24,7 @@ public class UpdateCheckManager extends BukkitRunnable implements xStartStop {
 
 	protected final long delay  = (new xTime( "5s")).ticks(50L);
 	protected final long loop   = (new xTime( "5m")).ticks(50L);
-	protected final long period = (new xTime("12h")).ticks(50L);
+	protected final long period = (new xTime("12h")).ms();
 
 	protected final AtomicLong lastCheck = new AtomicLong(0L);
 
