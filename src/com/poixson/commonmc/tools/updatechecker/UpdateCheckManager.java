7c4f236f0f074224687ebf66643ba795e0a41347
lorenzop <lorenzo@poixson.com>
2023-01-22 18:26:02 -0500

    removed duplicate cancel() call from UpdateCheckManager class

--- src/com/poixson/commonmc/tools/updatechecker/UpdateCheckManager.java
+++ src/com/poixson/commonmc/tools/updatechecker/UpdateCheckManager.java
@@ -56,7 +56,6 @@ public class UpdateCheckManager extends BukkitRunnable implements xStartStop {
 	}
 	@Override
 	public void stop() {
-		this.cancel();
 		this.listenerPlayerJoin.unregister();
 		try {
 			this.cancel();
