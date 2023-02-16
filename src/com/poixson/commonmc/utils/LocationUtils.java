205776537a5371d3bc06a3adefdd91269631fa9d
lorenzop <lorenzo@poixson.com>
2023-02-16 16:05:17 -0500

    renamed and cleaned up LocationUtils::Rotate() functions

--- src/com/poixson/commonmc/utils/LocationUtils.java
+++ src/com/poixson/commonmc/utils/LocationUtils.java
@@ -4,6 +4,7 @@ import org.bukkit.Axis;
 import org.bukkit.block.BlockFace;
 
 import com.poixson.tools.Keeper;
+import com.poixson.tools.dao.Ixy;
 import com.poixson.tools.dao.Ixywd;
 import com.poixson.tools.dao.Ixyz;
 
@@ -14,8 +15,13 @@ public final class LocationUtils {
 
 
 
-	public static Ixywd RotateXZ(final Ixywd loc, final BlockFace direction) {
-		switch (direction) {
+	// -------------------------------------------------------------------------------
+	// rotation
+
+
+
+	public static Ixywd Rotate(final Ixywd loc, final BlockFace face) {
+		switch (face) {
 		case NORTH: return loc;
 		case SOUTH: return new Ixywd(loc.w-loc.x-1, loc.d-loc.y-1, loc.w, loc.d);
 		case EAST:  return new Ixywd(loc.d-loc.y-1, loc.x,         loc.d, loc.w);
@@ -27,46 +33,37 @@ public final class LocationUtils {
 
 
 
-	public static BlockFace Rotate(final BlockFace direction, final double rotation) {
-		if (rotation < 0.0) throw new RuntimeException("Invalid rotation: "+Double.toString(rotation));
+	public static BlockFace Rotate(final BlockFace face, final double rotation) {
+		if (rotation < 0.0) throw new RuntimeException("Invalid rotation: " + Double.toString(rotation));
 		double rot = rotation % 1.0;
 		if (rot == 0.0)
-			return direction;
-		if (rot == 0.25) {
-			switch (direction) {
-			case NORTH: return BlockFace.EAST;
-			case EAST:  return BlockFace.SOUTH;
-			case SOUTH: return BlockFace.WEST;
-			case WEST:  return BlockFace.NORTH;
-			default: throw new RuntimeException("Invalid direction: "+direction.toString());
-			}
-		}
-		if (rot == 0.5) {
-			switch (direction) {
-			case NORTH: return BlockFace.SOUTH;
-			case EAST:  return BlockFace.WEST;
-			case SOUTH: return BlockFace.NORTH;
-			case WEST:  return BlockFace.EAST;
-			default: throw new RuntimeException("Invalid direction: "+direction.toString());
-			}
-		}
-		if (rot == 0.75) {
-			switch (direction) {
-			case NORTH: return BlockFace.WEST;
-			case EAST:  return BlockFace.NORTH;
-			case SOUTH: return BlockFace.EAST;
-			case WEST:  return BlockFace.SOUTH;
-			default: throw new RuntimeException("Invalid direction: "+direction.toString());
-			}
-		}
-		BlockFace dir = direction;
+			return face;
+		BlockFace dir = face;
 		while (rot > 0.0) {
 			switch (dir) {
 			case NORTH: dir = BlockFace.EAST;  break;
 			case EAST:  dir = BlockFace.SOUTH; break;
 			case SOUTH: dir = BlockFace.WEST;  break;
 			case WEST:  dir = BlockFace.NORTH; break;
-			default: throw new RuntimeException("Invalid direction: "+direction.toString());
+			default: throw new RuntimeException("Invalid direction: " + face.toString());
+			}
+			rot -= 0.25;
+		}
+		return dir;
+	}
+	public static char Rotate(final char ax, final double rotation) {
+		if (rotation < 0.0) throw new RuntimeException("Invalid rotation: " + Double.toString(rotation));
+		double rot = rotation % 1.0;
+		if (rot == 0.0)
+			return ax;
+		char dir = ax;
+		while (rot > 0.0) {
+			switch (dir) {
+			case 'n': dir = 'e'; break;
+			case 'e': dir = 's'; break;
+			case 's': dir = 'w'; break;
+			case 'w': dir = 'n'; break;
+			default: throw new RuntimeException("Invalid direction: " + dir);
 			}
 			rot -= 0.25;
 		}
