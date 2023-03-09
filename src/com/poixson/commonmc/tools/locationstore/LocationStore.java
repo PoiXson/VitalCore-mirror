bd9b111702bfb1ab0a2cdee291535ab44e5e6620
lorenzop <lorenzo@poixson.com>
2023-03-09 12:44:15 -0500

    added LocationStore classes - stores locations in 32x32 chunk areas

---
+++ src/com/poixson/commonmc/tools/locationstore/LocationStore.java
@@ -0,0 +1,143 @@
+package com.poixson.commonmc.tools.locationstore;
+
+import static com.poixson.commonmc.tools.plugin.xJavaPlugin.LOG;
+import static com.poixson.commonmc.tools.plugin.xJavaPlugin.LOG_PREFIX;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.poixson.tools.dao.Iab;
+import com.poixson.utils.Utils;
+
+
+public class LocationStore {
+
+	public static final int DELAY_SAVE   = 5;
+	public static final int DELAY_UNLOAD = 10;
+
+	protected final File file;
+
+	public final CopyOnWriteArraySet<Iab> locations = new CopyOnWriteArraySet<Iab>();
+
+	protected final AtomicBoolean changed = new AtomicBoolean(false);
+	protected final AtomicInteger state   = new AtomicInteger(0);
+
+
+
+	public LocationStore(final File file) {
+		this.file = file;
+	}
+
+
+
+	public void loadSafe(final int regionX, final int regionZ) {
+		try {
+			this.load(regionX, regionZ);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public boolean saveSafe() {
+		try {
+			return this.save();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public synchronized void load(final int regionX, final int regionZ) throws IOException {
+		this.locations.clear();
+		final BufferedReader reader = Files.newBufferedReader(this.file.toPath());
+		final Type token = new TypeToken<HashSet<String>>() {}.getType();
+		final Set<String> set = (new Gson()).fromJson(reader, token);
+		String[] split;
+		int x, z;
+		for (final String entry : set) {
+			try {
+				split = entry.split(",");
+				if (split.length != 2) throw new NumberFormatException();
+				x = Integer.parseInt(split[0].trim());
+				z = Integer.parseInt(split[1].trim());
+				this.locations.add(new Iab(x, z));
+			} catch (NumberFormatException e) {
+				LOG.warning(String.format(
+					"%sInvalid entry '%s' in file: %s",
+					LOG_PREFIX, entry, this.file.toString()
+				));
+				continue;
+			}
+		}
+	}
+
+	public boolean save() throws IOException {
+		if (this.changed.getAndSet(false)) {
+			final Set<String> result = new HashSet<String>();
+			final Iab[] set = this.locations.toArray(new Iab[0]);
+			for (final Iab loc : set)
+				result.add(loc.toString());
+			if (result.size() > 0) {
+				final String data = (new Gson()).toJson(result);
+				final BufferedWriter writer = new BufferedWriter(new FileWriter(this.file));
+				writer.write(data);
+				Utils.SafeClose(writer);
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+
+	// returns true if should unloaded
+	public boolean check() {
+		final int state = this.state.incrementAndGet();
+		// save
+		if (this.changed.get()) {
+			if (state == DELAY_SAVE)
+				this.saveSafe();
+			return false;
+		}
+		// unload
+		if (state > DELAY_UNLOAD)
+			return true;
+		return false;
+	}
+	public void markAccessed() {
+		this.state.set(0);
+	}
+
+
+
+	public boolean add(final int x, final int z) {
+		this.state.set(0);
+		final boolean result = this.locations.add(new Iab(x, z));
+		this.changed.set(true);
+		return result;
+	}
+	public boolean remove(final int x, final int z) {
+		this.state.set(0);
+		final boolean result = this.locations.remove(new Iab(x, z));
+		this.changed.set(true);
+		return result;
+	}
+	public boolean contains(final int x, final int z) {
+		this.state.set(0);
+		return this.locations.contains(new Iab(x, z));
+	}
+
+
+
+}
---
+++ src/com/poixson/commonmc/tools/locationstore/LocationStoreManager.java
@@ -0,0 +1,161 @@
+package com.poixson.commonmc.tools.locationstore;
+
+import static com.poixson.commonmc.tools.plugin.xJavaPlugin.LOG;
+import static com.poixson.commonmc.tools.plugin.xJavaPlugin.LOG_PREFIX;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.poixson.commonmc.utils.BukkitUtils;
+import com.poixson.tools.dao.Iab;
+
+
+public class LocationStoreManager extends BukkitRunnable {
+
+	protected final String type;
+	protected final File path;
+
+	protected final ConcurrentHashMap<Iab, LocationStore> regions = new ConcurrentHashMap<Iab, LocationStore>();
+
+
+
+	public LocationStoreManager(final String worldStr, final String type) {
+		this.type = type;
+		this.path = new File(BukkitUtils.GetServerPath(), worldStr+"/locs");
+		if (!this.path.isDirectory()) {
+			if (!this.path.mkdir())
+				throw new RuntimeException("Failed to create directory: " + this.path.toString());
+			LOG.info(  String.format("%sCreated directory: %s", LOG_PREFIX, this.path.toString()));
+		}
+	}
+
+
+
+	public LocationStoreManager start(final JavaPlugin plugin) {
+		this.runTaskTimerAsynchronously(plugin, 20L, 20L);
+		return this;
+	}
+
+
+
+	@Override
+	public void run() {
+		final Iterator<Entry<Iab, LocationStore>> it = this.regions.entrySet().iterator();
+		while (it.hasNext()) {
+			final Entry<Iab, LocationStore> entry = it.next();
+			if (entry.getValue().check())
+				this.regions.remove(entry.getKey());
+		}
+	}
+	public void saveAll() {
+		for (final LocationStore store : this.regions.values())
+			store.saveSafe();
+	}
+
+
+
+	public boolean add(final int x, final int z) {
+		final LocationStore region = this.getRegion(x, z);
+		return region.add(x, z);
+	}
+	public boolean remove(final int x, final int z) {
+		final LocationStore region = this.getRegion(x, z);
+		return region.remove(x, z);
+	}
+	public boolean contains(final int x, final int z) {
+		final LocationStore region = this.getRegion(x, z);
+		return region.contains(x, z);
+	}
+
+
+
+	public LocationStore getRegion(final int x, final int z) {
+		final int regionX = Math.floorDiv(x, 512);
+		final int regionZ = Math.floorDiv(z, 512);
+		final Iab loc = new Iab(regionX, regionZ);
+		// cached
+		{
+			final LocationStore store = this.regions.get(loc);
+			if (store != null) {
+				store.markAccessed();
+				return store;
+			}
+		}
+		// load existing or new
+		{
+			final String fileStr = String.format(
+				"%s.%d.%d.json",
+				this.type,
+				Integer.valueOf(regionX),
+				Integer.valueOf(regionZ)
+			);
+			final File file = new File(this.path, fileStr);
+			final LocationStore store = new LocationStore(file);
+			if (file.isFile())
+				store.loadSafe(regionX, regionZ);
+			final LocationStore existing = this.regions.putIfAbsent(loc, store);
+			if (existing == null)
+				return store;
+			existing.markAccessed();
+			return existing;
+		}
+	}
+
+
+
+	public Iab[] findNear(final int x, final int z) {
+		// single region
+		{
+			final LocationStore store = this.getRegion(x, z);
+			if (store != null) {
+				if (store.locations.size() > 0)
+					return store.locations.toArray(new Iab[0]);
+			}
+		}
+		// multiple regions
+		{
+			int xx, zz;
+			final HashSet<Iab> result = new HashSet<Iab>();
+			for (int iz=-1; iz<2; iz++) {
+				zz = (iz * 512) + z;
+				for (int ix=-1; ix<2; ix++) {
+					if (ix == 0 && iz == 0) continue;
+					xx = (ix * 512) + x;
+					final LocationStore store = this.getRegion(xx, zz);
+					for (final Iab loc : store.locations)
+						result.add(loc);
+				}
+			}
+			return result.toArray(new Iab[0]);
+		}
+	}
+	public Iab findNearest(final int x, final int z) {
+		final Iab[] near = this.findNear(x, z);
+		if (near.length > 0) {
+			double distance = Double.MAX_VALUE;
+			double dist;
+			Iab nearest = null;
+			for (final Iab loc : near) {
+				dist = Math.sqrt(
+					Math.pow( (double)(x-loc.a), 2.0) +
+					Math.pow( (double)(z-loc.b), 2.0)
+				);
+				if (distance > dist) {
+					distance = dist;
+					nearest  = loc;
+				}
+			}
+			return nearest;
+		}
+		return null;
+	}
+
+
+
+}
