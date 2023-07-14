55d31fedc61c3e2b898635a37891d7c6518b4367
lorenzop <lorenzo@poixson.com>
2023-07-14 09:22:07 -0400

    changed xListener to use generic JavaPlugin rather than xJavaPlugin

--- src/com/poixson/commonmc/tools/plugin/xListener.java
+++ src/com/poixson/commonmc/tools/plugin/xListener.java
@@ -3,9 +3,10 @@ package com.poixson.commonmc.tools.plugin;
 import org.bukkit.Bukkit;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
 
 
-public abstract class xListener<T extends xJavaPlugin> implements Listener {
+public abstract class xListener<T extends JavaPlugin> implements Listener {
 
 	protected final T plugin;
 
