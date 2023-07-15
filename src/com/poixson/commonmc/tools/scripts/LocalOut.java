a77d799a5cd81f6e5632ad95d4abd4bd8d1da678
lorenzop <lorenzo@poixson.com>
2023-07-15 02:46:33 -0400

    added LocalOut class - to broadcast script print() calls to nearby players

---
+++ src/com/poixson/commonmc/tools/scripts/LocalOut.java
@@ -0,0 +1,66 @@
+package com.poixson.commonmc.tools.scripts;
+
+import java.io.OutputStream;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import com.poixson.utils.StringUtils;
+
+
+public class LocalOut extends OutputStream {
+
+	protected final Location loc;
+
+	protected final StringBuilder buffer = new StringBuilder();
+
+
+
+	public LocalOut(final Location loc) {
+		super();
+		this.loc = loc;
+	}
+
+
+
+	@Override
+	public void write(final int chr) {
+		if (chr == '\r') return;
+		if (chr == '\n') {
+			if (this.buffer.length() == 0)
+				this.buffer.append(' ');
+			this.flush();
+		} else {
+			this.buffer.append((char)chr);
+		}
+	}
+
+
+
+	public void flush() {
+		String str = this.buffer.toString();
+		this.buffer.setLength(0);
+		str = StringUtils.ceTrim(str, '\n');
+		if (str.isEmpty())
+			str = " ";
+		final World world = this.loc.getWorld();
+		Location loc_player;
+		double distance;
+		for (final Player player : Bukkit.getOnlinePlayers()) {
+			loc_player = player.getLocation();
+			if (loc_player.getWorld().equals(world)) {
+				distance = loc_player.distance(this.loc);
+				if (distance < 10.0)
+					player.sendMessage(str);
+			}
+		}
+	}
+	public void close() {
+		this.flush();
+	}
+
+
+
+}
