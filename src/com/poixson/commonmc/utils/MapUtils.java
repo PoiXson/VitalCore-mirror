40caa305e317fd9acbdaada7595dd878a4b80fe4
lorenzop <lorenzo@poixson.com>
2023-07-18 00:22:43 -0400

    added MapUtils::DrawImagePixels() function with a Color mask

--- src/com/poixson/commonmc/utils/MapUtils.java
+++ src/com/poixson/commonmc/utils/MapUtils.java
@@ -45,6 +45,30 @@ public final class MapUtils {
 			} // end LOOP_X
 		} // end LOOP_Y
 	}
+	public static void DrawImagePixels(final Color[][] pixels,
+			final int x, final int y, final BufferedImage img,
+			final Color mask) {
+		final int w = pixels[0].length - 1;
+		final int h = pixels.length    - 1;
+		final int img_w = img.getWidth(null);
+		final int img_h = img.getHeight(null);
+		int xx, yy;
+		Color c;
+		//LOOP_Y:
+		for (int iy=0; iy<img_h; iy++) {
+			yy = iy + y;
+			LOOP_X:
+			for (int ix=0; ix<img_w; ix++) {
+				xx = ix + x;
+				if (xx < 0 || xx > w
+				||  yy < 0 || yy > h)
+					continue LOOP_X;
+				c = new Color(img.getRGB(ix, iy));
+				if (!mask.equals(c))
+					pixels[yy][xx] = c;
+			} // end LOOP_X
+		} // end LOOP_Y
+	}
 
 
 
