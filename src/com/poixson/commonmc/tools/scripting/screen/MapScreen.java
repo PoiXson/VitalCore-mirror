bfc24034411430abd3d5517071314dd20605ba51
lorenzop <lorenzo@poixson.com>
2023-07-18 00:20:55 -0400

    moving DrawImagePixels() functions from MapScreen class to MapUtils

--- src/com/poixson/commonmc/tools/scripting/screen/MapScreen.java
+++ src/com/poixson/commonmc/tools/scripting/screen/MapScreen.java
@@ -163,40 +163,6 @@ public class MapScreen extends MapRenderer implements Runnable, Closeable {
 
 
 
-	public static void DrawImagePixels(final Color[][] pixels,
-			final BufferedImage img, final int x, final int y) {
-		DrawImagePixels(pixels, img, null, x, y);
-	}
-	public static void DrawImagePixels(final Color[][] pixels,
-			final BufferedImage img, final BufferedImage mask,
-			final int x, final int y) {
-		final int w = pixels[0].length - 1;
-		final int h = pixels.length    - 1;
-		final int img_w = img.getWidth(null);
-		final int img_h = img.getHeight(null);
-		int xx, yy;
-		for (int iy=0; iy<img_h; iy++) {
-			yy = iy + y;
-			for (int ix=0; ix<img_w; ix++) {
-				xx = ix + x;
-				if (xx >= 0 && xx < w
-				&&  yy >= 0 && yy < h) {
-					if (mask == null
-					|| Color.WHITE.equals(new Color(mask.getRGB(ix, iy))))
-						pixels[iy+y][ix+x] = new Color(img.getRGB(ix, iy));
-//TODO: remove this?
-//System.out.println( img.getColorModel().hasAlpha() ? "YES" : "NO" );
-//					if (!Color.BLACK.equals(color))
-//					if (color.getTransparency() == Transparency.OPAQUE)
-//System.out.println(color.getAlpha());
-//					if (color.getAlpha() >= 255)
-				}
-			}
-		}
-	}
-
-
-
 	public Iabcd findScreenSize() {
 		int min_x = Integer.MAX_VALUE;
 		int min_y = Integer.MAX_VALUE;
--- src/com/poixson/commonmc/utils/MapUtils.java
+++ src/com/poixson/commonmc/utils/MapUtils.java
@@ -1,6 +1,7 @@
 package com.poixson.commonmc.utils;
 
 import java.awt.Color;
+import java.awt.image.BufferedImage;
 
 import org.bukkit.map.MapPalette;
 
@@ -13,6 +14,40 @@ public final class MapUtils {
 	@SuppressWarnings("deprecation")
 	public static Color NearestMapColor(final Color color) {
 		return MapPalette.getColor(MapPalette.matchColor(color));
+	}
+
+
+
+	public static void DrawImagePixels(final Color[][] pixels,
+			final BufferedImage img, final int x, final int y) {
+		DrawImagePixels(pixels, img, null, x, y);
+	}
+	public static void DrawImagePixels(final Color[][] pixels,
+			final BufferedImage img, final BufferedImage mask,
+			final int x, final int y) {
+		final int w = pixels[0].length - 1;
+		final int h = pixels.length    - 1;
+		final int img_w = img.getWidth(null);
+		final int img_h = img.getHeight(null);
+		int xx, yy;
+		for (int iy=0; iy<img_h; iy++) {
+			yy = iy + y;
+			for (int ix=0; ix<img_w; ix++) {
+				xx = ix + x;
+				if (xx >= 0 && xx < w
+				&&  yy >= 0 && yy < h) {
+					if (mask == null
+					|| Color.WHITE.equals(new Color(mask.getRGB(ix, iy))))
+						pixels[iy+y][ix+x] = new Color(img.getRGB(ix, iy));
+//TODO: remove this?
+//System.out.println( img.getColorModel().hasAlpha() ? "YES" : "NO" );
+//					if (!Color.BLACK.equals(color))
+//					if (color.getTransparency() == Transparency.OPAQUE)
+//System.out.println(color.getAlpha());
+//					if (color.getAlpha() >= 255)
+				}
+			}
+		}
 	}
 
 
