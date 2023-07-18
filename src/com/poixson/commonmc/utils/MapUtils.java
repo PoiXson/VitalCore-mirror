2b39873a98c1651eb248741f09ac841e7f4a6b06
lorenzop <lorenzo@poixson.com>
2023-07-18 00:22:01 -0400

    cleanup and fixes to MapUtils::DrawImagePixels() functions

--- src/com/poixson/commonmc/utils/MapUtils.java
+++ src/com/poixson/commonmc/utils/MapUtils.java
@@ -19,35 +19,31 @@ public final class MapUtils {
 
 
 	public static void DrawImagePixels(final Color[][] pixels,
-			final BufferedImage img, final int x, final int y) {
-		DrawImagePixels(pixels, img, null, x, y);
+			final int x, final int y, final BufferedImage img) {
+		DrawImagePixels(pixels, x, y, img, (BufferedImage)null);
 	}
 	public static void DrawImagePixels(final Color[][] pixels,
-			final BufferedImage img, final BufferedImage mask,
-			final int x, final int y) {
+			final int x, final int y, final BufferedImage img,
+			final BufferedImage mask) {
 		final int w = pixels[0].length - 1;
 		final int h = pixels.length    - 1;
 		final int img_w = img.getWidth(null);
 		final int img_h = img.getHeight(null);
 		int xx, yy;
+		//LOOP_Y:
 		for (int iy=0; iy<img_h; iy++) {
 			yy = iy + y;
+			LOOP_X:
 			for (int ix=0; ix<img_w; ix++) {
 				xx = ix + x;
-				if (xx >= 0 && xx < w
-				&&  yy >= 0 && yy < h) {
-					if (mask == null
-					|| Color.WHITE.equals(new Color(mask.getRGB(ix, iy))))
-						pixels[iy+y][ix+x] = new Color(img.getRGB(ix, iy));
-//TODO: remove this?
-//System.out.println( img.getColorModel().hasAlpha() ? "YES" : "NO" );
-//					if (!Color.BLACK.equals(color))
-//					if (color.getTransparency() == Transparency.OPAQUE)
-//System.out.println(color.getAlpha());
-//					if (color.getAlpha() >= 255)
-				}
-			}
-		}
+				if (xx < 0 || xx > w
+				||  yy < 0 || yy > h)
+					continue LOOP_X;
+				if (mask != null && !Color.WHITE.equals(new Color(mask.getRGB(ix, iy))))
+					continue LOOP_X;
+				pixels[yy][xx] = new Color(img.getRGB(ix, iy));
+			} // end LOOP_X
+		} // end LOOP_Y
 	}
 
 
