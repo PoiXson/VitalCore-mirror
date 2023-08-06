6c213bc1de0399abb9c8470ec2d7af7a5edc53a6
lorenzop <lorenzo@poixson.com>
2023-08-06 15:02:12 -0400

    added more feature flags to ScriptContextFactory->hasFeature()

--- src/com/poixson/commonmc/tools/scripting/engine/ScriptContextFactory.java
+++ src/com/poixson/commonmc/tools/scripting/engine/ScriptContextFactory.java
@@ -37,12 +37,28 @@ public class ScriptContextFactory extends ContextFactory {
 	@Override
 	protected boolean hasFeature(final Context context, final int featureIndex) {
 		switch (featureIndex) {
-			case Context.FEATURE_STRICT_MODE:
-			case Context.FEATURE_STRICT_VARS:
-			case Context.FEATURE_ENABLE_JAVA_MAP_ACCESS:
-			case Context.FEATURE_LOCATION_INFORMATION_IN_ERROR:
-				return true;
-			default: break;
+		case Context.FEATURE_STRICT_MODE:
+		case Context.FEATURE_STRICT_VARS:
+		case Context.FEATURE_STRICT_EVAL:
+		case Context.FEATURE_V8_EXTENSIONS:
+		case Context.FEATURE_ENABLE_JAVA_MAP_ACCESS:
+		case Context.FEATURE_INTEGER_WITHOUT_DECIMAL_PLACE:
+		case Context.FEATURE_ENABLE_XML_SECURE_PARSING:
+		case Context.FEATURE_LOCATION_INFORMATION_IN_ERROR:
+		case Context.FEATURE_WARNING_AS_ERROR:
+		case Context.FEATURE_MEMBER_EXPR_AS_FUNCTION_NAME:
+		case Context.FEATURE_PARENT_PROTO_PROPERTIES:
+		case Context.FEATURE_OLD_UNDEF_NULL_THIS:
+			return true;
+		case Context.FEATURE_RESERVED_KEYWORD_AS_IDENTIFIER:
+		case Context.FEATURE_TO_STRING_AS_SOURCE:
+		case Context.FEATURE_DYNAMIC_SCOPE:
+		case Context.FEATURE_ENHANCED_JAVA_ACCESS:
+		case Context.FEATURE_ENUMERATE_IDS_FIRST:
+		case Context.FEATURE_THREAD_SAFE_OBJECTS:
+		case Context.FEATURE_LITTLE_ENDIAN:
+			return false;
+		default: break;
 		}
 		return super.hasFeature(context, featureIndex);
 	}
