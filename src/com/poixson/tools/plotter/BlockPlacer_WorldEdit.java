123f1a29a18be6e24ca0881a89562c32558b52c5
lorenzop <lorenzo@poixson.com>
2024-01-07 02:30:28 -0500

    added more setBlockData() and getType() functions to BlockPlacer_WorldEdit for better cross-placer support

--- src/com/poixson/tools/plotter/BlockPlacer_WorldEdit.java
+++ src/com/poixson/tools/plotter/BlockPlacer_WorldEdit.java
@@ -1,5 +1,7 @@
 package com.poixson.tools.plotter;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.block.data.BlockData;
 
 import com.sk89q.worldedit.EditSession;
@@ -28,22 +30,36 @@ public class BlockPlacer_WorldEdit extends BlockPlacer {
 
 
 
+	// block data
 	@Override
 	public BlockData getBlock(final int x, final int y, final int z) {
 		return BukkitAdapter.adapt(
 			this.session.getBlock(this.origin.add(x, y, z))
 		);
 	}
+	public void setBlockData(final int x, final int y, final int z, final BlockData block) {
+		this.setBlock(x, y, z, block);
+	}
 	@Override
-	public void setBlock(final int x, final int y, final int z, final BlockData type) {
+	public void setBlock(final int x, final int y, final int z, final BlockData block) {
 		try {
 			this.session.setBlock(
 				this.origin.add(x, y, z),
-				BukkitAdapter.adapt(type)
+				BukkitAdapter.adapt(block)
 			);
 		} catch (MaxChangedBlocksException e) {
 			e.printStackTrace();
 		}
+	}
+
+	// material
+	public Material getType(final int x, final int y, final int z) {
+		final BlockData block = this.getBlock(x, y, z);
+		return (block == null ? null : block.getMaterial());
+	}
+	public void setType(final int x, final int y, final int z, final Material type) {
+		final BlockData block = Bukkit.createBlockData(type);
+		this.setBlock(x, y, z, block);
 	}
 
 
