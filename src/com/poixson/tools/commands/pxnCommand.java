f29df7adfe07e72a9a843a19fb65d8fe7bed941b
lorenzop <lorenzo@poixson.com>
2024-04-28 00:15:08 -0400

    rewrote pxnCommand classes - simpler and can register commands at runtime

--- src/com/poixson/tools/commands/pxnCommand.java
+++ src/com/poixson/tools/commands/pxnCommand.java
@@ -1,17 +1,93 @@
 package com.poixson.tools.commands;
 
+import static com.poixson.utils.Utils.IsEmpty;
+
+import java.util.LinkedList;
+import java.util.List;
+
 import org.bukkit.command.CommandSender;
 
 
-public interface xCMD {
+public class pxnCommand {
+
+	protected final String[] labels;
+
+	protected final LinkedList<pxnCommand> children = new LinkedList<pxnCommand>();
 
 
-	public boolean match(final String label);
-	public String[] getLabels();
 
-	public boolean hasOverrides();
+	public pxnCommand(final String...labels) {
+		this.labels = labels;
+	}
 
-	public boolean run(final CommandSender sender, final String[] args);
+
+
+	// override for end point
+	public boolean onCommand(final CommandSender sender, final String[] args) {
+		final LinkedList<String> list = new LinkedList<String>();
+		for (final String label : args)
+			list.addLast(label);
+		final String first = list.removeFirst();
+		for (final pxnCommand cmd : this.children) {
+			if (cmd.match(first) != null)
+				return cmd.onCommand(sender, list.toArray(new String[0]));
+		}
+		return false;
+	}
+
+
+
+	public List<String> onTabComplete(final CommandSender sender, final String[] args) {
+		if (IsEmpty(args)) return null;
+		final LinkedList<String> found = new LinkedList<String>();
+		final LinkedList<String> list = new LinkedList<String>();
+		for (final String arg : args)
+			list.addLast(arg);
+		final String first = list.removeFirst();
+
+		if (list.isEmpty()) {
+			for (final pxnCommand command : this.children) {
+				final String match = command.matchPart(first);
+				if (match != null)
+					found.addLast(match);
+			}
+		} else {
+			for (final pxnCommand command : this.children) {
+				if (command.match(first) != null) {
+					final List<String> result = command.onTabComplete(sender, list.toArray(new String[0]));
+					if (result != null) {
+						for (final String str : result)
+							found.addLast(str);
+					}
+				}
+			}
+		}
+		return found;
+	}
+
+
+
+	public void addCommand(final pxnCommand command) {
+		this.children.addLast(command);
+	}
+
+
+
+	public String match(final String match) {
+		for (final String label : this.labels) {
+			if (label.equals(match))
+				return label;
+		}
+		return null;
+	}
+	public String matchPart(final String match) {
+		for (final String label : this.labels) {
+			if (label.startsWith(match))
+				return label;
+		}
+		return null;
+	}
+
 
 
 }
--- src/com/poixson/tools/commands/pxnCommandRoot.java
+++ src/com/poixson/tools/commands/pxnCommandRoot.java
@@ -1,114 +1,52 @@
 package com.poixson.tools.commands;
 
-import java.util.LinkedList;
+import static com.poixson.utils.BukkitUtils.GetCommand;
+
+import java.io.Closeable;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArraySet;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.command.TabCompleter;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import com.poixson.tools.xJavaPlugin;
-import com.poixson.tools.events.xListener;
 
 
-public class xCMD_Handler implements xCMD, xListener, CommandExecutor, TabCompleter {
+public class pxnCommandRoot extends pxnCommand
+implements CommandExecutor, TabCompleter, Closeable {
 
-	protected final xJavaPlugin plugin;
-
-	protected final LinkedList<xCMD> commands = new LinkedList<xCMD>();
-
-	protected final CopyOnWriteArraySet<PluginCommand> pcs = new CopyOnWriteArraySet<PluginCommand>();
+	public final PluginCommand plugin_command;
 
 
 
-	public xCMD_Handler(final xJavaPlugin plugin) {
-		this.plugin = plugin;
-	}
-
-
-
-	public void register() {
-		this.register(this.plugin);
-	}
-	@Override
-	public void register(final JavaPlugin plugin) {
-		if (this.hasOverrides())
-			xListener.super.register(plugin);
-		for (final String label : this.getLabels()) {
-			final PluginCommand pc = plugin.getCommand(label);
-			if (pc == null) {
-				this.plugin.log().warning("Command not found: "+label);
-				continue;
-			}
-			pc.setExecutor(this);
-			this.pcs.add(pc);
-		}
-	}
-	@Override
-	public void unregister() {
-		xListener.super.unregister();
-		for (final PluginCommand pc : this.pcs)
-			pc.setExecutor(null);
-		this.pcs.clear();
+	public pxnCommandRoot(final xJavaPlugin plugin,
+			final String desc, final String usage,
+			final String...labels) {
+		super();
+		this.plugin_command = GetCommand(plugin, null, labels, desc, usage);
+		this.plugin_command.setExecutor(this);
 	}
 
 
 
 	@Override
-	public boolean onCommand(final CommandSender sender, final Command command,
-			final String label, final String[] args) {
-		return this.run(sender, args);
-	}
-
-	@Override
-	public boolean run(final CommandSender sender, final String[] args) {
-		for (final xCMD cmd : this.commands) {
-			if (cmd.run(sender, args))
-				return true;
-		}
-		return false;
-	}
-
-
-
-	public void addCommand(final xCMD cmd) {
-		this.commands.addLast(cmd);
-	}
-	@Override
-	public boolean hasOverrides() {
-		return false;
+	public void close() {
+		this.plugin_command.setExecutor(null);
 	}
 
 
 
 	@Override
-	public String[] getLabels() {
-		final LinkedList<String> labels = new LinkedList<String>();
-		for (final xCMD cmd : this.commands) {
-			for (final String label : cmd.getLabels())
-				labels.add(label);
-		}
-		return labels.toArray(new String[0]);
+	public boolean onCommand(final CommandSender sender,
+			final Command command, final String label, final String[] args) {
+		return this.onCommand(sender, args);
 	}
-
 	@Override
-	public boolean match(final String label) {
-		for (final xCMD cmd : this.commands) {
-			if (cmd.match(label))
-				return true;
-		}
-		return false;
-	}
-
-
-
-	@Override
-	public List<String> onTabComplete(final CommandSender sender, final Command command,
-			final String label, final String[] args) {
+	public List<String> onTabComplete(final CommandSender sender,
+			final Command command, final String label, final String[] args) {
+		return this.onTabComplete(sender, args);
 	}
 
 
