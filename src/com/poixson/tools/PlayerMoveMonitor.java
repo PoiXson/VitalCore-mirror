9dcb0d5af7325af2e969f9ed56069a94d94fc6e8
lorenzop <lorenzo@poixson.com>
2024-07-10 01:17:03 -0400

    added AFKPlayerEvent

--- src/com/poixson/tools/PlayerMoveMonitor.java
+++ src/com/poixson/tools/PlayerMoveMonitor.java
@@ -1,5 +1,7 @@
 package com.poixson.tools;
 
+import static com.poixson.utils.BukkitUtils.SafeCancel;
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map.Entry;
@@ -15,8 +17,12 @@ import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import com.poixson.tools.events.AFKPlayerEvent;
 import com.poixson.tools.events.OutsideOfWorldEvent;
 import com.poixson.tools.events.OutsideOfWorldEvent.OutsideOfWorld;
 import com.poixson.tools.events.PlayerMoveNormalEvent;
@@ -27,11 +33,36 @@ public class PlayerMoveMonitor implements xListener {
 	public static final int WORLD_MIN_Y = -64;
 	public static final int WORLD_MAX_Y = 319;
 
-	public final ConcurrentHashMap<UUID, LocationSafe> locations = new ConcurrentHashMap<UUID, LocationSafe>();
+	protected final ConcurrentHashMap<UUID, LocationSafe> locations = new ConcurrentHashMap<UUID, LocationSafe>();
+
+	protected final HashMap<UUID, Long> afk = new HashMap<UUID, Long>();
+
+	protected final BukkitRunnable task_afk;
+	protected final long afk_interval = 20L;  // 1 second
+	protected final long afk_thresh   = 900L; // 15 minutes
 
 
 
 	public PlayerMoveMonitor() {
+		this.task_afk = new BukkitRunnable() {
+			@Override
+			public void run() {
+				PlayerMoveMonitor.this.updateAFK();
+			}
+		};
+	}
+
+
+
+	@Override
+	public void register(final JavaPlugin plugin) {
+		xListener.super.register(plugin);
+		this.task_afk.runTaskTimer(plugin, 40L, this.afk_interval);
+	}
+	@Override
+	public void unregister() {
+		xListener.super.unregister();
+		SafeCancel(this.task_afk);
 	}
 
 
@@ -54,16 +85,57 @@ public class PlayerMoveMonitor implements xListener {
 
 
 
+	// -------------------------------------------------------------------------------
+	// afk
+
+
+
+	public void updateAFK() {
+		//LOOP_AFK:
+		for (final UUID uuid : this.afk.keySet()) {
+			final Long sleeping = this.afk.get(uuid);
+			if (sleeping != null) {
+				final long slept = sleeping.longValue() + 1L;
+				this.afk.put(uuid, Long.valueOf(slept));
+				// go afk
+				if (slept == this.afk_thresh) {
+					final AFKPlayerEvent event = new AFKPlayerEvent(false, uuid);
+					Bukkit.getPluginManager().callEvent(event);
+				}
+			}
+		} // end LOOP_AFK
+	}
+
+	public void voidAFK(final UUID uuid) {
+		final Long sleeping = this.afk.replace(uuid, Long.valueOf(0L));
+		if (sleeping != null) {
+			final long slept = sleeping.longValue();
+			// go active
+			if (slept >= this.afk_thresh) {
+				final AFKPlayerEvent event = new AFKPlayerEvent(false, uuid);
+				Bukkit.getPluginManager().callEvent(event);
+			}
+		}
+	}
+
+
+
+	// -------------------------------------------------------------------------------
+	// listeners
+
+
+
 	@EventHandler(priority=EventPriority.NORMAL, ignoreCancelled=true)
 	public void onPlayerMove(final PlayerMoveEvent event) {
+		final Player player = event.getPlayer();
+		final UUID uuid = player.getUniqueId();
+		this.voidAFK(uuid);
 		final Location from = event.getFrom();
 		final Location to   = event.getTo();
 		if ( (from.getBlockX() != to.getBlockX())
 		||   (from.getBlockY() != to.getBlockY())
 		||   (from.getBlockZ() != to.getBlockZ()) ) {
 			final PluginManager pm = Bukkit.getPluginManager();
-			final Player player = event.getPlayer();
-			final UUID uuid = player.getUniqueId();
 			{
 				final LocationSafe loc = this.locations.get(uuid);
 				if (loc == null) {
@@ -108,6 +180,7 @@ public class PlayerMoveMonitor implements xListener {
 	public void onPlayerChangeWorld(final PlayerChangedWorldEvent event) {
 		final Player player = event.getPlayer();
 		final UUID uuid = player.getUniqueId();
+		this.voidAFK(uuid);
 		final LocationSafe loc = new LocationSafe(player.getLocation());
 		this.locations.put(uuid, loc);
 	}
@@ -118,6 +191,7 @@ public class PlayerMoveMonitor implements xListener {
 	public void onPlayerJoin(final PlayerJoinEvent event) {
 		final Player player = event.getPlayer();
 		final UUID uuid = player.getUniqueId();
+		this.afk.put(uuid, Long.valueOf(0L));
 		this.locations.put(uuid, new LocationSafe(player.getLocation()));
 	}
 
@@ -130,8 +204,10 @@ public class PlayerMoveMonitor implements xListener {
 				list.add(uuid);
 			for (final Player player : Bukkit.getOnlinePlayers())
 				list.remove(player.getUniqueId());
-			for (final UUID uuid : list)
+			for (final UUID uuid : list) {
+				this.afk.remove(uuid);
 				this.locations.remove(uuid);
+			}
 		}
 	}
 
---
+++ src/com/poixson/tools/events/AFKPlayerEvent.java
@@ -0,0 +1,71 @@
+package com.poixson.tools.events;
+
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+
+public class AFKPlayerEvent extends Event implements Cancellable {
+
+	private static final HandlerList handlers = new HandlerList();
+
+	protected final AtomicBoolean cancel = new AtomicBoolean(false);
+
+	protected final boolean isAFK;
+
+	protected final UUID uuid;
+
+
+
+	public AFKPlayerEvent(final boolean isAFK, final UUID uuid) {
+		this.isAFK = isAFK;
+		this.uuid  = uuid;
+	}
+
+
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+
+
+
+	@Override
+	public boolean isCancelled() {
+		return this.cancel.get();
+	}
+	@Override
+	public void setCancelled(final boolean cancel) {
+		this.cancel.set(cancel);
+	}
+
+
+
+	public boolean isAFK() {
+		return this.isAFK;
+	}
+	public boolean isActive() {
+		return ! this.isAFK;
+	}
+
+
+
+	public Player getPlayer() {
+		return Bukkit.getPlayer(this.uuid);
+	}
+	public UUID getUUID() {
+		return this.uuid;
+	}
+
+
+
+}
