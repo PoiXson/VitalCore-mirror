33eed6ccfb9424311a3b0a535f44d8d6195eee94
lorenzop <lorenzo@poixson.com>
2024-11-17 17:22:01 -0500

    simplify LocationUtils::FaceToAxisString() function

--- src/com/poixson/utils/LocationUtils.java
+++ src/com/poixson/utils/LocationUtils.java
@@ -288,19 +288,7 @@ public final class LocationUtils {
 	}
 
 	public static String FaceToAxisString(final BlockFace face) {
-		switch (face) {
-		case UP:    return "up";
-		case DOWN:  return "down";
-		case NORTH: return "north";
-		case SOUTH: return "south";
-		case EAST:  return "east";
-		case WEST:  return "west";
-		case NORTH_EAST: return "north-east";
-		case NORTH_WEST: return "north-west";
-		case SOUTH_EAST: return "south-east";
-		case SOUTH_WEST: return "south-west";
-		default: return null;
-		}
+		return face.toString().toLowerCase();
 	}
 
 	public static char FaceToPillarAx(final BlockFace face) {
