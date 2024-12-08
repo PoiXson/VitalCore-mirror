77a1d68f8f0d8fbf70b413a8bc1c87612bef48b1
lorenzop <lorenzo@poixson.com>
2024-12-08 18:52:37 -0500

    cleanup and improvements in GsonAdapter_BlockData class

--- src/com/poixson/utils/gson/GsonAdapter_BlockData.java
+++ src/com/poixson/utils/gson/GsonAdapter_BlockData.java
@@ -3,10 +3,13 @@ package com.poixson.utils.gson;
 import java.io.IOException;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.block.data.BlockData;
 
+import com.google.gson.GsonBuilder;
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
 
@@ -14,11 +17,21 @@ public class GsonAdapter_BlockData extends TypeAdapter<BlockData> {
 
 
 
-	public void write(final JsonWriter out, final BlockData block) throws IOException {
-		out.jsonValue(block.getAsString());
+	public static void Register(final GsonBuilder builder) {
+		builder.registerTypeAdapter(Location.class, new GsonAdapter_Location());
+		builder.registerTypeAdapter(BlockData.class, new GsonAdapter_BlockData());
 	}
+
+
+
 	public BlockData read(final JsonReader in) throws IOException {
+		if (in.peek() == JsonToken.NULL) { in.nextNull(); return null; }
 		return Bukkit.createBlockData(in.nextString());
+	}
+
+	public void write(final JsonWriter out, final BlockData block) throws IOException {
+		if (block == null) { out.nullValue(); return; }
+		out.jsonValue(block.getAsString());
 	}
 
 
